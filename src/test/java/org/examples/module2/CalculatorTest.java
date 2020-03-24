9c8f4c942fc752ba042e5d391d861e60d03cf54e
Petru Flueras <petru.flueras@gmail.com>
2020-03-24 18:32:03 +0200

    Added a test for calculator

---
+++ src/test/java/org/examples/module2/CalculatorTest.java
@@ -0,0 +1,19 @@
+package org.examples.module2;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest {
+    private AdvancedCalculator calculator;
+
+    @Before
+    public void setUp() {
+        calculator = new AdvancedCalculator();
+    }
+
+    @Test
+    public void calculatorTest() {
+        Assert.assertEquals(12, calculator.multuply(3, 4));
+    }
+}
